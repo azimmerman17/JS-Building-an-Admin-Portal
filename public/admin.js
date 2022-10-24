39ba147c0611b1c74dfb52faa4e04a688bb4e0bb
Andrew Zimmerman <azimmerman@pga.com>
2022-10-24 17:41:26 -0400

    used GET and PATCH methods to create admon page to chage book quanities

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function admin () {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(bookList)
+};
 
-// Your Code Here
+function bookList(book) {
+    let root = document.getElementById('root')
+
+    let li = document.createElement('li')
+        li.textContent = book.title
+
+    let input = document.createElement('input')
+        input.value = book.quantity
+
+    let saveBtn = document.createElement('button')
+        saveBtn.textContent = 'Save'
+
+    saveBtn.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+               'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value,
+            }),
+        })
+    })
+
+    li.append(input, saveBtn)
+    root.append(li)
+
+};
+
+admin()
